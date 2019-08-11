b807db507a7757713e5adc37240d0bb1283815c8
rj1995-dev <ram95.jana@gmail.com>
2019-08-11 10:23:38 +0530

    add person list

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from "react-dom";
 import "./index.css";
 const Person = ({ img, name, job, children }) => {
   const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
+  // const url = `../Images/sample_pic_${img}.jpg`;
   return (
     <article className="person">
       <img src={url} alt="person" />
@@ -15,7 +16,7 @@ const Person = ({ img, name, job, children }) => {
 const PersonList = () => {
   return (
     <section className="person-list">
-      <Person img="30" name="Ramchandra" job="developer">
+      <Person img="11" name="Ramchandra" job="developer">
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
           accusamus?
@@ -27,6 +28,23 @@ const PersonList = () => {
         </p>
       </Person>
       <Person img="36" name="suman Kylan" job="tester">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit,
+          inventore.
+        </p>
+      </Person>
+      <Person img="31" name="Akash" job="the boss">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
+          accusamus?
+        </p>
+      </Person>
+      <Person img="40" name="Rajesh" job="hr">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, fuga.
+        </p>
+      </Person>
+      <Person img="50" name="kingshuk" job="tester">
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit,
           inventore.
